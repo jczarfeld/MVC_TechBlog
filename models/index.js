0e398b81f98abf47129834039ae19d20d799af1f
John Osterfeld <jczarfeld@gmail.com>
2021-08-02 09:15:53 -0400

    made the models and started the server and connection. need to add cookies and auth

---
+++ models/index.js
@@ -0,0 +1,44 @@
+
+const Comment = require('./Comment');
+const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks: true
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+    hooks:true
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+
+
+
+
+module.exports = { User, Post, Comment };
