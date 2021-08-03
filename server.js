02af9418ee6ada667a83e7d83acdc5bd25b7ffc8
John Osterfeld <jczarfeld@gmail.com>
2021-08-03 16:48:09 -0400

    started working on the handlebars and I added the utils helpers for auth and date/time

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 const path = require('path');
+require('dotenv').config();
 const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+
+    secret: process.env.DB_SESSION_SECRET,
+    cookie: { maxAge: 7200000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session(sess));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+    app.listen(PORT, () => console.log('Now listening'));
+  });
